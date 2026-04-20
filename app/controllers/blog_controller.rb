a405d421e11629be2304cf6862640710fbd8a022
danielschia <daniel.schia@gmail.com>
2026-04-20 17:01:45 -0300

    projeto finalizado

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,7 @@
 class BlogController < ApplicationController
+  layout 'application_blank'
+
   def index
-    @posts = Post.where('publication_date <= ?', Time.current).order(publication_date: :desc)
+    @posts = Post.where('publication_date <= ?', Time.current).order(publication_date: :desc).first
   end
 end
